140cbaa6dda9e863b5c7a6112d218fbf01ccb7e9
José Ulombe <jose.ulombe@hotmail.com>
2020-10-02 21:04:01 +0100

    chore: add routes

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
-const express = require('express')
-const nunjucks = require('nunjucks')
+const express = require('express')  //importando a biblioteca do express
+const nunjucks = require('nunjucks') //importando a biblioteca do nunjucks
 
 const server = express()
+
+//configure template engine(nunjucks)
+server.set("view engine", "njk")   //setar qual é o motor de views da app, qual é a extensão dos arquivos para abrir 
+nunjucks.configure("views", {
+    express: server,               //indica ao nunjucks que vamos usar o Express com a var Server
+    autoescape: false,             //impede que o nunjucks mostre o codigo html em variaveis
+    noCache: true                  //bloqueando o cache do nunjucks         
+    
+})
+
+
+// create route
+server.get("/", function(req, res) {
+    return res.render("index")
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //start server
